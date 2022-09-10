7ba4456fece9d2fcf656d6a28722bf896f337e57
Leslie Machuca #14-1°C <113222310+lesliemachuca21@users.noreply.github.com>
2022-09-09 22:03:09 -0600

    Add files via upload
    Leslie Gisselle Machuca Amaya #14 -1°C

---
+++ Unidad2Semana4Clase12Ejercicio3.java
@@ -0,0 +1,19 @@
+
+package unidad2semana4clase12ejercicio3;
+
+/**
+ *
+ * @author Leslie Gisselle Machuca Amaya
+ */
+public class Unidad2Semana4Clase12Ejercicio3 {
+
+    public static void main(String[] args) {
+      
+         int suma = 0;
+         for (int i=100; i<=200; i++ ){
+        suma = suma +i;
+    }
+   System.out.println("La suma es:" + suma);
+    }
+    
+}
