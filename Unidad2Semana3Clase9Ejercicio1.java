0eecdd70190d8ea4f8402bc2557821161f83e674
Leslie Machuca #14-1°C <113222310+lesliemachuca21@users.noreply.github.com>
2022-09-09 22:02:26 -0600

    Add files via upload
    Leslie Gisselle Machuca Amaya #14 -1°C

---
+++ Unidad2Semana3Clase9Ejercicio1.java
@@ -0,0 +1,15 @@
+
+package Unidad2Semana3Clase9Ejercicio1;
+
+public class Unidad2Semana3Clase9Ejercicio1 {
+
+    public static void main(String[] args) {
+        int i=0;
+        while(true){//Condicion trivial :siempre cierta
+            i++;
+            System.out.println("Valor de i: "+i);
+            if(i==9){break;}
+        }
+    }
+    
+}
