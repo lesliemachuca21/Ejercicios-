97330de0f1e4406a8132bf5f619a449e953389f2
Leslie Machuca #14-1°C <113222310+lesliemachuca21@users.noreply.github.com>
2022-09-09 22:24:27 -0600

    Add files via upload

---
+++ Unidad3Semana3Clase7Ejercicio5.java
@@ -0,0 +1,20 @@
+
+package unidad3semana3clase7ejercicio1;
+
+
+/**
+ *
+ * @author Leslie Gisselle Machuca Amaya
+ */
+public class Unidad3Semana3Clase7Ejercicio1 {
+
+    public static void main(String[] args) {
+         int i = 0;
+      while (true) {
+          i++;
+           System.out.println("Valor de i: " + i);
+           if (i==9) {break;}
+         }
+    }
+    
+}
