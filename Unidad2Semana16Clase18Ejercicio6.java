07f1f818170e9abb8ab24d35130ff47d79cc6a7d
Leslie Machuca #14-1°C <113222310+lesliemachuca21@users.noreply.github.com>
2022-09-09 22:07:44 -0600

    Add files via upload

---
+++ Unidad2Semana16Clase18Ejercicio6.java
@@ -0,0 +1,16 @@
+
+package unidad2semana16clase18ejercicio6;
+
+/**
+ *
+ * @author Leslie Gisselle Machuca Amaya
+ */
+public class Unidad2Semana16Clase18Ejercicio6 {
+
+    public static void main(String[] args) {
+          for (char c='a'; c<='z'; c++) {
+            System.out.println("El caracter" + (int)c + "es:" + c);
+        }
+    }
+    
+}
