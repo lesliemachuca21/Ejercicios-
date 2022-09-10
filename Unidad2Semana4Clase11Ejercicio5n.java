9f53426a51b3992ec5f710876f768a833e534cdd
Leslie Machuca #14-1°C <113222310+lesliemachuca21@users.noreply.github.com>
2022-09-09 22:07:23 -0600

    Add files via upload
    Leslie Gisselle Machuca Amaya #14 1°C

---
+++ Unidad2Semana4Clase11Ejercicio5n.java
@@ -0,0 +1,21 @@
+
+package unidad2semana4clase11ejercicio5n;
+
+/**
+ *
+ * @author Leslie Gisselle Machuca Amaya
+ */
+public class Unidad2Semana4Clase11Ejercicio5n {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         String cadena="";
+        for (int i=1; i<=20; i++ ){
+            cadena = cadena + "*";
+            System.out.println(cadena);
+    }
+    
+    }
+}
